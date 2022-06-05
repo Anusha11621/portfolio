c9749ef8b3123fdc5bc3125afd01a84b06f8fd97
Anusha <anusha.kadambala61@gmailcom>
2022-06-05 18:12:53 +0530

    navbar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+      path: 'about',
+      component: AboutComponent
+  },
+  {
+      path: 'education',
+      component: EducationComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'project',
+    component: ProjectComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+    
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
